01cd5c6f1ae01f70eed61e1d82d18bd7a5cbc172
Saebom Kim <rlatoqha0101@naver.com>
2022-09-28 17:20:47 +0900

    express

--- 04.Express/views/tigers-template.js
+++ 04.Express/views/tigers-template.js
@@ -1,5 +1,5 @@
 module.exports = {
-    home: function(trs) {
+    home : function(trs) {
         return `
         <!DOCTYPE html>
         <html lang="en">
@@ -9,17 +9,21 @@ module.exports = {
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>기아 타이거즈</title>
             <style>
-                th, tr { text-align: center }
+                th, tr {
+                    text-align : center
+                }
+                img { height : 50px;}
             </style>
         </head>
-        <body style="margin: 50px;">
-            <h1>기아 타이거즈 선수단</h1>
+        <body style="margin:50px">
+            <img src="/tigers/emblem.jpg" alt="로고" style = "height: 250px;">
             <button onclick="location.href='/create'">추가</button>
             <hr>
             <table>
                 <tr>
                     <th>ID</th>
                     <th>선수명</th>
+                    <th>사진</th>
                     <th>백넘버</th>
                     <th>포지션</th>
                     <th>액션</th>
@@ -28,23 +32,20 @@ module.exports = {
             </table>
         </body>
         </html>
-        `;
+        `
     },
-
     trsGen: function(rows) {
         let trs = '';
         for (let row of rows) {
             trs += '<tr>';
-            trs += `<td>${row.id}</td><td>${row.player}</td>`;
-            trs += `<td>${row.backNo}</td><td>${row.position}</td>`;
-            trs += `<td><a href="/update?id=${row.id}">수정</a>, 
+            trs += `<td>${row.id}</td><td>${row.player}</td><td><img src="/tigers/${row.player}.jpg"></td><td>${row.backNo}</td><td>${row.position}</td>`;
+            trs += `<td><a href="/update?id=${row.id}">수정</a>,
                         <a href="/delete?id=${row.id}">삭제</a></td>`;
             trs += '</tr>';
         }
-        return trs;
+        return trs
     },
-
-    createForm: function() {
+    createForm : function() {
         return `
         <!DOCTYPE html>
         <html lang="en">
@@ -54,14 +55,16 @@ module.exports = {
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>기아 타이거즈</title>
             <style>
-                th, tr { text-align: center }
+                th, tr {
+                    text-align : center
+                }
             </style>
         </head>
-        <body style="margin: 50px;">
+        <body style="margin:50px">
             <h1>기아 타이거즈 선수단</h1>
             <button onclick="location.href='/'">홈으로</button>
             <hr>
-            <form action="/create" method="post">
+            <form action="/create" method="POST">
                 <table>
                     <tr>
                         <td>선수명</td><td><input type="text" name="player"></td>
@@ -81,8 +84,7 @@ module.exports = {
         </html>
         `;
     },
-
-    updateForm: function(id, player, backNo, position) {
+    updateForm : function(id, player, backNo, position) {
         return `
         <!DOCTYPE html>
         <html lang="en">
@@ -92,24 +94,26 @@ module.exports = {
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>기아 타이거즈</title>
             <style>
-                th, tr { text-align: center }
+                th, tr {
+                    text-align : center
+                }
             </style>
         </head>
-        <body style="margin: 50px;">
+        <body style="margin:50px">
             <h1>기아 타이거즈 선수단</h1>
             <button onclick="location.href='/'">홈으로</button>
             <hr>
-            <form action="/update" method="post">
+            <form action="/update" method="POST">
                 <input type="hidden" name="id" value="${id}">
                 <table>
                     <tr>
-                        <td>선수명</td><td><input type="text" name="player" value="${player}"></td>
+                        <td>선수명</td><td><input type="text" name="player" value=${player}></td>
                     </tr>
                     <tr>
-                        <td>백넘버</td><td><input type="text" name="backNo" value="${backNo}"></td>
+                        <td>백넘버</td><td><input type="text" name="backNo" value=${backNo}></td>
                     </tr>
                     <tr>
-                        <td>포지션</td><td><input type="text" name="position" value="${position}"></td>
+                        <td>포지션</td><td><input type="text" name="position" value=${position}></td>
                     </tr>
                     <tr>
                         <td colspan="2"><input type="submit" value="수정"></td>
@@ -123,7 +127,7 @@ module.exports = {
     deleteForm: function(id) {
         return `
         <!DOCTYPE html>
-        <html lang="ko">
+        <html lang="en">
         <head>
             <meta charset="UTF-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -143,3 +147,14 @@ module.exports = {
         `;
     }
 }
+// //
+//     trsGen: function(rows) {            // rows를 가지고 trs를 만들기   // 객체는 forloop으로 돌려서 받는다.
+//         let trs = '';                   // 빈스트링을 만든다.
+//         for (let row of rows) {         // row를 가지고 반복문을 해서 빈스트링에 넣는다?
+//             trs += '<tr>';              // 줄시작 때 작성
+//             trs += `<td>${row.id}</td><td>${row.player}</td><td>${row.backNO}</td><td>${row.position}`;
+//             trs += '</tr>';
+//         }
+//         return trs;
+//     }
+// }
